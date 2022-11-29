634976a8cad131a3bdc48d4631e8bd75356e2853
unknown <osama_magdy0@yahoo.com>
2022-11-29 22:07:52 +0200

    The second upload from Home

---
+++ index.js
@@ -0,0 +1,10 @@
+let time = new Date();
+let timenow = new Date(01 - 01 - 2023);
+
+const year = time.getFullYear();
+const Day = time.getDate();
+const Hour = time.getHours();
+const min = time.getMinutes();
+const sec = time.getSeconds();
+
+// function theTime() {}
