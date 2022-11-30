6ac807c0ebd83088ff0b3eb949eb48c43beec805
unknown <osama_magdy0@yahoo.com>
2022-11-30 08:13:31 +0200

    the last update

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-let time = new Date();
-let timenow = new Date(01 - 01 - 2023);
+const DEl = document.querySelector(".D");
+const HEl = document.querySelector(".H");
+const MEl = document.querySelector(".M");
+const SEl = document.querySelector(".S");
 
-const year = time.getFullYear();
-const Day = time.getDate();
-const Hour = time.getHours();
-const min = time.getMinutes();
-const sec = time.getSeconds();
+// const year = time.getFullYear();
+// const Day = time.getDate();
+// const Hour = time.getHours();
+// const min = time.getMinutes();
+// const sec = time.getSeconds();
+let newYear = new Date("Jan 1, 2023 00:00:00");
 
-// function theTime() {}
+function updateTime() {
+  const now = new Date();
+  const gab = newYear - now;
+  DEl.innerHTML = `${Math.floor(gab / 1000 / 60 / 60 / 24)}`; // days
+  HEl.innerHTML = `${24 - now.getHours()}`;
+  MEl.innerHTML = `${60 - now.getMinutes() - 1}`;
+  SEl.innerHTML = `${60 - now.getSeconds() - 1}`;
+
+  setTimeout(updateTime, 1000);
+}
+updateTime();
+
+// console.log(gab / 1000); // second
+// console.log(gab / 1000 / 60); // mins
+// console.log(gab / 1000 / 60 / 60); //hours
+// console.log(gab / 1000 / 60 / 60 / 24); // days
+// console.log(gab / 1000 / 60 / 60 / 24 / 365); // years
